bddc59683d7b66820a6405571cfbb9d4341aab08
Irza Orvala Alfandi <orvala9@gmail.com>
2025-12-10 19:54:59 +0700

    a

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -1,12 +1,10 @@
 import Wrapper from "../layout/wrapper";
 import SEO from "../components/common/seo";
-import FooterElement from "../components/elements/footer-element";
 
 const index = () => {
   return (
     <Wrapper>
       <SEO pageTitle={"Footer Element"} />
-      <FooterElement />
     </Wrapper>
   );
 };
